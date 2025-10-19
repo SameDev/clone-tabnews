ae6c8cf1c0a78d16e78fd138cf2f418063706677
SameDev <conradtsamuel>
2025-10-19 18:11:09 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Se você me ama, mande uma mensagem escrita com a coisa que você mais ama em mim!</h1>;
+    return <h1>Se você me ama, mande uma mensagem escrita com a coisa que você mais ama em mim! 🖤</h1>;
 }
 
 export default Home;
